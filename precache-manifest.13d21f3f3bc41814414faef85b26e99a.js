ce75de57a737a7feafbb8da49e520912ddf646ca
Alessia Trianti <alestrianti@gmail.com>
2020-07-29 14:37:17 +0200

    Updates

---
+++ precache-manifest.13d21f3f3bc41814414faef85b26e99a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd29c2241d23c018e25a5260d2adfa2e",
+    "url": "/ztm-robofriends/index.html"
+  },
+  {
+    "revision": "4b5f6fa28317ff1032c5",
+    "url": "/ztm-robofriends/static/css/main.ee33c7ab.chunk.css"
+  },
+  {
+    "revision": "2439406d0ddfe557ce5c",
+    "url": "/ztm-robofriends/static/js/2.580e9fb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ztm-robofriends/static/js/2.580e9fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b5f6fa28317ff1032c5",
+    "url": "/ztm-robofriends/static/js/main.4943711d.chunk.js"
+  },
+  {
+    "revision": "fc644a321fb87cb9c413",
+    "url": "/ztm-robofriends/static/js/runtime-main.597edd97.js"
+  }
+]);
